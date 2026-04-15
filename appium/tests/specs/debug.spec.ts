a6fff0bb4e2b0c8cdc91451fc232d58ec20492dc
Fadi George <fadii925@gmail.com>
2026-04-15 11:46:07 -0700

    test(appium): remove debug test file

--- appium/tests/specs/debug.spec.ts
+++
@@ -1,9 +0,0 @@
-describe.skip('Debug', () => {
-  it('log ui hierarchy', async () => {
-    const html = await driver.getPageSource();
-    console.log(html);
-  });
-  // it('can debug', async () => {
-  //   await scrollToTop();
-  // });
-});
